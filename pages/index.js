48ec9e7fa9e0848d4566fd627114cbf9879d3e45
Quan Tran <tran.q90@gmail.com>
2021-04-29 16:53:43 -0700

    wrapping entire app with component

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,28 @@
-import MeetupList from '../components/meetups/MeetupList';
+import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
-    {
-        id: 'm1',
-        title: 'A first Meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Mcgraw-hill-42nd-st_1.jpg/800px-Mcgraw-hill-42nd-st_1.jpg',
-        address: '1234 address, address',
-        description: 'first meetup'
-    },
-    {
-        id: 'm2',
-        title: 'A second Meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Mcgraw-hill-42nd-st_1.jpg/800px-Mcgraw-hill-42nd-st_1.jpg',
-        address: '1234 address, address',
-        description: 'second meetup'
-    }
-]
+  {
+    id: "m1",
+    title: "A first Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Mcgraw-hill-42nd-st_1.jpg/800px-Mcgraw-hill-42nd-st_1.jpg",
+    address: "1234 address, address",
+    description: "first meetup",
+  },
+  {
+    id: "m2",
+    title: "A second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Mcgraw-hill-42nd-st_1.jpg/800px-Mcgraw-hill-42nd-st_1.jpg",
+    address: "1234 address, address",
+    description: "second meetup",
+  },
+];
 
 function HomePage() {
-    return(
-        <MeetupList meetups={DUMMY_MEETUPS} />
-    )
+  return (
+      <MeetupList meetups={DUMMY_MEETUPS} />
+  );
 }
 
 export default HomePage;
