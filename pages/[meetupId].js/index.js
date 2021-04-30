659871b5531b4ebe7726e167def82403b1238c05
Quan Tran <tran.q90@gmail.com>
2021-04-29 20:08:31 -0700

    getStaticProps

--- pages/[meetupId].js/index.js
+++
@@ -1,5 +0,0 @@
-function MeetupDetail() {
-  return <h1>The Meetup Detail Page</h1>;
-}
-
-export default MeetupDetail;
---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      img="https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Mcgraw-hill-42nd-st_1.jpg/800px-Mcgraw-hill-42nd-st_1.jpg"
+      title="first meetup"
+      address="asdasd123"
+      description="asdasdasd"
+    />
+  );
+}
+
+export default MeetupDetails;
